452fff1064e35b63be2f86d0167b7ed1087e7f75
Petr Plsek <me@kalanys.com>
2022-02-20 10:38:30 +0100

    kw_paging - add translations interface

--- php-tests/BasicTests/BasicTest.php
+++ php-tests/BasicTests/BasicTest.php
@@ -4,6 +4,8 @@ namespace BasicTests;
 
 
 use CommonTestClass;
+use kalanis\kw_paging\Render\THelpingText;
+use kalanis\kw_paging\Render\Translations;
 
 
 class BasicTest extends CommonTestClass
@@ -20,4 +22,20 @@ class BasicTest extends CommonTestClass
         $position->getPager()->setActualPage($position->getLastPage());
         $this->assertFalse($position->nextPageExists());
     }
+
+    public function testTranslations(): void
+    {
+        $position = $this->getPositions();
+        $position->getPager()->setActualPage(4);
+        $lib = new HelpingText();
+        $this->assertEmpty($lib->getFilledText($position));
+        $lib->setLang(new Translations());
+        $this->assertNotEmpty($lib->getFilledText($position));
+    }
+}
+
+
+class HelpingText
+{
+    use THelpingText;
 }
